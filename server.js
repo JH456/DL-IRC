252305abeea0278f366683c70d89b8a0fb2cd934
Jim <jharris9456@gmail.com>
2018-01-16 22:59:54 -0500

    make server.js a little cleaner

--- server.js
+++ server.js
@@ -1,35 +1,41 @@
 let botModule = require('./bot_modules')
 
-let ircConfig = {
-    botName: process.env.IRC_BOT_NAME,
-    nickservMessage: 'identify ' + process.env.IRC_NICKSERV_PASS,
-    servers: [
-        {
-            serverName: process.env.IRC_SERVER_NAME,
-            channels: [
-                process.env.IRC_CHANNEL_NAME
-            ]
-        }
-    ],
-    botModule: 'irc',
-    commandPrefix: '*',
-    userModules: [
-        'youtube',
-        'silly'
-    ]
+let configs = {
+    "irc": {
+        botName: process.env.IRC_BOT_NAME,
+        nickservMessage: 'identify ' + process.env.IRC_NICKSERV_PASS,
+        servers: [
+            {
+                serverName: process.env.IRC_SERVER_NAME,
+                channels: [
+                    process.env.IRC_CHANNEL_NAME
+                ]
+            }
+        ],
+        botModule: 'irc',
+        commandPrefix: '*',
+        userModules: [
+            'youtube',
+            'silly'
+        ]
+    },
+    "slack": {
+        apiToken: process.env.SLACK_API_TOKEN,
+        botModule: 'slack',
+        commandPrefix: '+',
+        userModules: [
+            'youtube',
+            'silly'
+        ]
+    }
 }
 
-let slackConfig = {
-    apiToken: process.env.SLACK_API_TOKEN,
-    botModule: 'slack',
-    commandPrefix: '+',
-    userModules: [
-        'youtube',
-        'silly'
-    ]
+let configName = process.env.BOT_MODULE
+let config = configs[configName]
+if (config) {
+    console.log("Starting a(n) " + configName + " bot.")
+    let bot = botModule.create(config)
+    botModule.addListenersTo(bot, config)
+} else {
+    console.log("Error! Invalid bot module!")
 }
-
-let config = process.env.BOT_MODULE === 'slack' ? slackConfig : ircConfig
-
-let bot = botModule.create(config)
-botModule.addListenersTo(bot, config)
